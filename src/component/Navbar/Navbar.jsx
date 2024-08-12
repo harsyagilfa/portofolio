9612f6a8eb2759bea2d01d6da1c91e39cd8540d8
harsyagilfa <gilangharsya85@hmail.com>
2024-08-12 17:18:29 +0700

    add: initial files

--- src/component/Navbar/Navbar.jsx
+++ src/component/Navbar/Navbar.jsx
@@ -24,12 +24,12 @@ const Navbar = () =>
             <img className="menu-open" src={menu_open} onClick={openMenu} alt="" />
             <ul ref={menuRef} className="nav-menu" >
                 <img className="menu-close" src={menu_close} onClick={closeMenu} alt="" />
-                <li><AnchorLink className='anchor-link' href='#home' ><p onClick={()=>setMenu("home")} >Home</p></AnchorLink>{menu==='home'?<img src={underline} alt="" />:<></>}</li>   
-                <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>About</p></AnchorLink>{menu==='about'?<img src={underline} alt="" />:<></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#skills'><p onClick={()=>setMenu("skills")}>Skills</p></AnchorLink>{menu==='skills'?<img src={underline} alt="" />:<></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#certificate'><p onClick={()=>setMenu("certificate")}>Certificate</p></AnchorLink>{menu==='certificate'?<img src={underline} alt="" />:<></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#project'><p onClick={()=>setMenu("project")}>Project</p></AnchorLink>{menu==='project'?<img src={underline} alt="" />:<></>}</li>
-                <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setMenu("contact")}>Contact</p></AnchorLink>{menu==='contact'?<img src={underline} alt="" />:<></>}</li>
+                <li><AnchorLink className='anchor-link' href='#home' ><p onClick={()=>setMenu("home")} >HOME</p></AnchorLink>{menu==='home'?<img src={underline} alt="" />:<></>}</li>   
+                <li><AnchorLink className='anchor-link' offset={50} href='#about'><p onClick={()=>setMenu("about")}>ABOUT</p></AnchorLink>{menu==='about'?<img src={underline} alt="" />:<></>}</li>
+                <li><AnchorLink className='anchor-link' offset={50} href='#skills'><p onClick={()=>setMenu("skills")}>SKILLS</p></AnchorLink>{menu==='skills'?<img src={underline} alt="" />:<></>}</li>
+                <li><AnchorLink className='anchor-link' offset={50} href='#certificate'><p onClick={()=>setMenu("certificate")}>CERTIFICATE</p></AnchorLink>{menu==='certificate'?<img src={underline} alt="" />:<></>}</li>
+                <li><AnchorLink className='anchor-link' offset={50} href='#project'><p onClick={()=>setMenu("project")}>PROJECT</p></AnchorLink>{menu==='project'?<img src={underline} alt="" />:<></>}</li>
+                <li><AnchorLink className='anchor-link' offset={50} href='#contact'><p onClick={()=>setMenu("contact")}>CONTACT</p></AnchorLink>{menu==='contact'?<img src={underline} alt="" />:<></>}</li>
             </ul>
             <div className="nav-connect"><a href="https://www.linkedin.com/in/gilangharsya/">CONNECT ME</a></div>
         </div>
